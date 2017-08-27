25e7adda634b209849d990b7480a92cd14b59c0c
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2017-08-26 21:17:14 -0500

    Todo empezo un 26 de agosto..

---
+++ SPOJ/TEST - Life, the Universe, and Everything/submit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+	int s;
+	bool bandera = true;
+	while(cin>>s){
+		if(s==42)bandera = false;
+		
+		if(bandera){
+			cout<<s<<endl;
+		}
+		
+ 
+	}
+	// your code here
+ 
+	return 0;
+}
+//Juan Cadavid.
